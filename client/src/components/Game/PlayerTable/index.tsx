63324560ff6cfd53bf34270266edb290c9ce1dec
alexhans <mail@alexanderhans.me>
2020-10-24 15:56:52 +0200

    added player  avatars

--- client/src/components/Game/PlayerTable/index.tsx
+++ client/src/components/Game/PlayerTable/index.tsx
@@ -1,40 +1,92 @@
 import React from "react";
-import { calculateBettingRoundSpendingForPlayer } from "../helpers";
-import { Player, BettingRound } from "../../../interfaces";
+import {
+  calculateBettingRoundSpendingForPlayer,
+  isPlayerDead,
+} from "../helpers";
+import { Player, BettingRound, QuestionRound } from "../../../interfaces";
+
+import "./styles.scss";
 
 interface PlayerTableProps {
   players?: Player[];
   playerId?: Player["id"];
   currentBettingRound?: BettingRound;
+  currentQuestionRound?: QuestionRound;
 }
+
+const moveAppPlayerToTop = (players: Player[], playerId: Player["id"]) =>
+  players.splice(
+    0,
+    0,
+    players.splice(
+      players.findIndex(({ id }) => id === playerId),
+      1
+    )[0]
+  );
 
 export default ({
   players,
   playerId,
   currentBettingRound,
+  currentQuestionRound,
 }: PlayerTableProps) => {
-  if (!players?.length) {
+  if (!players?.length || !playerId) {
     return null;
   }
+  moveAppPlayerToTop(players, playerId);
+
   return (
     <div>
-      <p>Players:</p>
-      {(players || []).map(({ id, money }, i) => (
-        <div className="ml-3" key={id}>
-          {i !== 0 && <hr />}
-          <div>
-            {id === playerId && <span>👩‍💻</span>}Name: {id}
-            {currentBettingRound?.currentPlayer.id === id && <span>❗️</span>}
-          </div>
-          <div>Remaining money: {money}</div>
-          {currentBettingRound && (
-            <div>
-              Amount bet:{" "}
-              {calculateBettingRoundSpendingForPlayer(currentBettingRound, id)}
+      {(players || []).map(({ id, money }) => {
+        const isDead =
+          currentQuestionRound &&
+          isPlayerDead(currentQuestionRound, { id, money });
+        return (
+          <div key={id} className="d-flex align-items-center pt-4 ml-4">
+            <div
+              className={`avatar ${id === playerId ? "lg" : "md"} ${
+                isDead ? "dead" : ""
+              }`}
+            >
+              <span>🧟‍♂️</span>
+              {currentBettingRound?.currentPlayer.id === id && (
+                <span className="dice" role="img" aria-label="dice">
+                  🎲
+                </span>
+              )}
             </div>
-          )}
-        </div>
-      ))}
+            <div>
+              {id === playerId ? (
+                <div
+                  className={`money ${id === playerId ? "" : "md"} ${
+                    isDead ? "dead" : ""
+                  }`}
+                >
+                  <span>300km</span>
+                  <span role="img" aria-label="money">
+                    💰{money}
+                  </span>
+                </div>
+              ) : (
+                <div
+                  className={`money ${id === playerId ? "" : "md"} ${
+                    isDead ? "dead" : ""
+                  }`}
+                >
+                  <span role="img" aria-label="money">
+                    💰{money}
+                  </span>
+                </div>
+              )}
+            </div>
+            {isDead && (
+              <span className="skull" role="img" aria-label="skull">
+                💀
+              </span>
+            )}
+          </div>
+        );
+      })}
     </div>
   );
 };
