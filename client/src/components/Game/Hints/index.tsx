4186ba5b548935563509cfb93782784c7ab949f4
alexhans <mail@alexanderhans.me>
2020-10-24 19:36:49 +0200

    styled the game  hints

--- client/src/components/Game/Hints/index.tsx
+++ client/src/components/Game/Hints/index.tsx
@@ -1,30 +1,48 @@
 import React from "react";
 import { QuestionRound } from "../../../interfaces";
 
+const styles = {
+  title: {
+    borderTop: "1px solid #ebebeb",
+    marginTop: "0.4em",
+  },
+  currentHint: {
+    fontSize: "1.5em",
+  },
+  oldHint: {
+    fontSize: "0.7em",
+  },
+};
+
 interface HintsProps {
   currentQuestionRound: QuestionRound;
 }
 
 export default ({ currentQuestionRound }: HintsProps) => {
   const hints = currentQuestionRound.question.hints;
-  if (currentQuestionRound.bettingRounds.length <= 1) {
+  const numberOfHints = currentQuestionRound.bettingRounds.length - 1;
+  if (numberOfHints < 1) {
     return null;
   }
   if (!hints?.length) {
     return null;
   }
   return (
-    <>
-      <p>Hints:</p>
+    <div className="d-flex flex-column">
+      <span style={styles.title}>
+        Hint{numberOfHints > 1 && "s"} ({numberOfHints}/{hints.length}):
+      </span>
       {currentQuestionRound.question.hints
         .slice(0, currentQuestionRound.bettingRounds.length - 1)
         .map((hint, i) => (
-          <p>
-            <b>
-              {i + 1}. {hint}
-            </b>
-          </p>
+          <span
+            style={
+              numberOfHints === i + 1 ? styles.currentHint : styles.oldHint
+            }
+          >
+            {hint}
+          </span>
         ))}
-    </>
+    </div>
   );
 };
