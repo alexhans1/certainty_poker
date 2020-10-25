00a91cc35c65016875a67837b87ce4f02bbe1fba
alexhans <mail@alexanderhans.me>
2020-10-25 13:47:52 +0100

    added generic drawer component

--- client/src/components/Game/AnswerDrawer/index.tsx
+++ client/src/components/Game/AnswerDrawer/index.tsx
@@ -1,9 +1,7 @@
 import React, { useState } from "react";
-import Drawer from "@material-ui/core/Drawer";
+import Drawer from "../../Drawer";
 import { Game, Player, QuestionRound } from "../../../interfaces";
 import { AddGuess, addGuess, isPlayerDead } from "../helpers";
-
-import "./styles.scss";
 
 interface QuestionProps {
   game: Game;
@@ -24,58 +22,49 @@ export default ({
     return null;
   }
   const [guess, setGuess] = useState<number>();
-  const [isCollapsed, setIsCollapsed] = useState(false);
   const canAddGuess = !currentQuestionRound.guesses.find(
     (guess) => guess.playerId === playerId
   );
   return (
     <Drawer
+      title="New Question"
+      isCollapseAble={true}
+      shouldBeCollapsed={canAddGuess}
       anchor={"bottom"}
       open={canAddGuess}
       variant="persistent"
-      className={`drawer ${isCollapsed && canAddGuess ? "collapsed" : ""}`}
     >
-      <div className="d-flex align-items-center flex-column">
-        <span id="newQuestion" className="w-100">
-          <button
-            onClick={() => {
-              setIsCollapsed(!isCollapsed);
+      <>
+        <p>{currentQuestionRound.question.question}</p>
+        <div className="input-group mb-3">
+          <input
+            value={guess}
+            onChange={(e) => {
+              setGuess(parseFloat(e.target.value) || 0);
             }}
-          >
-            New Question
-          </button>
-        </span>
-        <div className="container px-5 pt-4 pb-5 d-flex flex-column">
-          <p>{currentQuestionRound.question.question}</p>
-          <div className="d-flex justify-content-center">
-            <input
-              value={guess}
-              onChange={(e) => {
-                setGuess(parseFloat(e.target.value) || 0);
-              }}
-              disabled={!canAddGuess}
-              type="number"
-              className="form-control form-control-lg"
-              placeholder="Your answer"
-              aria-label="Your answer"
-              aria-describedby="basic-addon2"
-            />
-
+            disabled={!canAddGuess}
+            type="number"
+            className="form-control form-control-lg"
+            placeholder="Your answer"
+            aria-label="Your answer"
+            aria-describedby="basic-addon2"
+          />
+          <div className="input-group-append">
             <button
               type="submit"
-              className="submitButton btn btn-primary"
-              disabled={!canAddGuess}
+              className="btn btn-primary"
+              disabled={!canAddGuess || (!guess && guess !== 0)}
               onClick={(e) => {
                 if (guess || guess === 0) {
                   addGuess(addGuessMutation, game, guess, playerId);
                 }
               }}
             >
-              Submit
+              ⮑
             </button>
           </div>
         </div>
-      </div>
+      </>
     </Drawer>
   );
 };
