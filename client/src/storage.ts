b6423ce97d2901ccd46a2426e1776da404e6045f
alexhans <mail@alexanderhans.me>
2020-10-27 10:40:54 +0100

    save playerId and gameId in local rather session storage

--- client/src/storage.ts
+++ client/src/storage.ts
@@ -1,8 +1,8 @@
 const PLAYER_ID_KEY = "player_id";
 
 export const setPlayerIdToStorage = (gameId: string, playerId: string) => {
-  sessionStorage.setItem(`${PLAYER_ID_KEY}_${gameId}`, playerId);
+  localStorage.setItem(`${PLAYER_ID_KEY}_${gameId}`, playerId);
 };
 
 export const getPlayerIdFromStorage = (gameId: string) =>
-  sessionStorage.getItem(`${PLAYER_ID_KEY}_${gameId}`);
+  localStorage.getItem(`${PLAYER_ID_KEY}_${gameId}`);
