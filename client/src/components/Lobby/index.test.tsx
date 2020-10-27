fa45d51d735a8f154986f8e5632e3a879a1ebd66
alexhans <mail@alexanderhans.me>
2020-10-27 19:14:09 +0100

    removed unused tests

--- client/src/components/Lobby/index.test.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import { render } from "@testing-library/react";
-import Lobby from ".";
-
-test("renders learn react link", () => {
-  const { getByText } = render(<Lobby />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
