de59a6c9b330d9fb50b01aa365e94468cfd3af16
jonathan-scholbach <j.scholbach@posteo.de>
2020-10-27 16:55:52 +0100

    update Question, construct new Game with randomly drawn Question

--- server/graph/model/question.go
+++ server/graph/model/question.go
@@ -1,6 +1,10 @@
 package model
 
 import (
+	"os"
+	"io/ioutil"
+	"encoding/json"
+	"math/rand"
 	"github.com/alexhans1/certainty_poker/helpers"
 )
 
@@ -14,4 +18,36 @@ func NewQuestion(question string, answer float64, hints []string) *Question {
 	q.ID = helpers.CreateID()
 
 	return q
+}
+
+// Load Questions from json file
+func LoadQuestions() []Question {
+	questionsJson, _ := os.Open("questions.json")
+	
+	defer questionsJson.Close()
+	byteValue, _ := ioutil.ReadAll(questionsJson)
+
+	var allQuestions []Question
+	json.Unmarshal(byteValue, allQuestions)
+
+	return allQuestions
+}
+
+// Draw a random Question which is not burnt yets
+func DrawQuestion(burntQuestions []string) *Question {
+	allQuestions := LoadQuestions()
+	openQuestions := make([]Question, 0)
+
+	for _, question := range(allQuestions){
+		if helpers.ContainsString(burntQuestions, question.Question){
+			continue
+		} else {
+			openQuestions = append(openQuestions, question)
+		}
+	}
+
+	randomIndex := rand.Intn(len(openQuestions))
+    drawnQuestion := &openQuestions[randomIndex]
+
+    return drawnQuestion
 }
