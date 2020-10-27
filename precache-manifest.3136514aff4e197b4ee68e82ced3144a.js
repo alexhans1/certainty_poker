769a44be91baaa43db2ca5be0484e208c8c9b3e7
alexhans <mail@alexanderhans.me>
2020-10-27 19:34:30 +0100

    v1.0.0

---
+++ precache-manifest.3136514aff4e197b4ee68e82ced3144a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "825ca070d469eb3def5773fae3f6e54c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fca72277f93561b61de8",
+    "url": "/static/css/main.46f7a010.chunk.css"
+  },
+  {
+    "revision": "abe78418f1dcefb63283",
+    "url": "/static/js/2.f020d48e.chunk.js"
+  },
+  {
+    "revision": "882fee5ad56cfd78b1cc5d94b617aa42",
+    "url": "/static/js/2.f020d48e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fca72277f93561b61de8",
+    "url": "/static/js/main.3588b7fb.chunk.js"
+  },
+  {
+    "revision": "702554416a81a1e3a894",
+    "url": "/static/js/runtime-main.784c586e.js"
+  }
+]);
