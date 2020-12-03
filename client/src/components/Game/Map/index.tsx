7d2e7c90b1a86acc717c59df9ace0793598f6186
alexhans <mail@alexanderhans.me>
2020-12-03 23:13:27 +0100

    added fly to bounds transition to map

--- client/src/components/Game/Map/index.tsx
+++ client/src/components/Game/Map/index.tsx
@@ -1,11 +1,15 @@
-import React, { useState } from "react";
+import React, { ReactNode, useState } from "react";
+import { latLngBounds } from "leaflet";
 import {
   MapContainer,
   TileLayer,
   Marker,
   useMapEvents,
   Tooltip,
+  FeatureGroup,
+  useMap,
 } from "react-leaflet";
+import { equals } from "ramda";
 import { GeoCoordinate } from "../../../interfaces";
 
 import "./styles.scss";
@@ -30,35 +34,62 @@ function LocationMarker({ handleUpdate }: { handleUpdate: HandleOnClick }) {
     },
   });
 
-  return position === null ? null : <Marker position={position} />;
+  return position && <Marker position={position} />;
 }
 
-export default ({ markers, handleOnClick }: Props) => {
-  return (
-    <MapContainer
-      center={[0, 0]}
-      zoom={1}
-      scrollWheelZoom={true}
-      className="mb-5"
-    >
-      <TileLayer
-        attribution='Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-        url="https://stamen-tiles-{s}.a.ssl.fastly.net/watercolor/{z}/{x}/{y}.svg"
-      />
-      {handleOnClick && <LocationMarker handleUpdate={handleOnClick} />}
-      {markers?.length &&
-        markers.map(({ position, label }) => (
-          <Marker
-            position={[position.latitude, position.longitude]}
-            key={position.latitude}
-          >
-            {label && (
-              <Tooltip direction="bottom" offset={[-15, 20]} permanent>
-                {label}
-              </Tooltip>
-            )}
-          </Marker>
-        ))}
-    </MapContainer>
-  );
-};
+function MarkerContainer({
+  children,
+  markers,
+}: {
+  children: ReactNode;
+  markers: Marker[];
+}) {
+  const map = useMap();
+  const bounds = latLngBounds([]);
+  markers?.forEach((data) => {
+    bounds.extend([data.position.latitude, data.position.longitude]);
+  });
+  bounds.isValid() &&
+    map.flyToBounds(bounds, {
+      padding: [10, 10],
+      maxZoom: markers.length > 1 ? 15 : 5,
+    });
+
+  return <FeatureGroup>{children}</FeatureGroup>;
+}
+
+export default React.memo(
+  ({ markers = [], handleOnClick }: Props) => {
+    return (
+      <MapContainer
+        center={[0, 0]}
+        zoom={1}
+        scrollWheelZoom={true}
+        className="mb-5"
+      >
+        <TileLayer
+          attribution='Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url="https://stamen-tiles-{s}.a.ssl.fastly.net/watercolor/{z}/{x}/{y}.svg"
+        />
+        {handleOnClick && <LocationMarker handleUpdate={handleOnClick} />}
+        {markers.length && (
+          <MarkerContainer markers={markers}>
+            {markers.map(({ position, label }) => (
+              <Marker
+                position={[position.latitude, position.longitude]}
+                key={position.latitude}
+              >
+                {label && (
+                  <Tooltip direction="bottom" offset={[-15, 20]} permanent>
+                    {label}
+                  </Tooltip>
+                )}
+              </Marker>
+            ))}
+          </MarkerContainer>
+        )}
+      </MapContainer>
+    );
+  },
+  (prevProps, nextProps) => equals(prevProps.markers, nextProps.markers)
+);
