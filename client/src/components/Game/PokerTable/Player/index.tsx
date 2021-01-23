dd051b93512ee7e13a9a8c045ab17560e47bc7d5
alexhans <mail@alexanderhans.me>
2021-01-24 00:47:00 +0100

    added betting round spending to players

--- client/src/components/Game/PokerTable/Player/index.tsx
+++ client/src/components/Game/PokerTable/Player/index.tsx
@@ -1,20 +1,52 @@
 import React from "react";
-import { Player } from "../../../../interfaces";
+import { MonetizationOn } from "@material-ui/icons";
+import { BettingRound, Player } from "../../../../interfaces";
 
 import "./styles.scss";
+import { calculateBettingRoundSpendingForPlayer } from "../../helpers";
 
 interface Props {
   player: Player;
+  currentBettingRound?: BettingRound;
   index: number;
+  isTurnPlayer: boolean;
+  isAppPlayer: boolean;
+  isQuestionRoundOver: boolean;
 }
 
-export default ({ player, index }: Props) => {
+export default ({
+  player,
+  index,
+  isTurnPlayer,
+  isAppPlayer,
+  isQuestionRoundOver,
+  currentBettingRound,
+}: Props) => {
+  const isTurnPlayerClass = isTurnPlayer ? "isTurnPlayer" : "";
+  const isAppPlayerClass = isAppPlayer ? "isAppPlayer" : "";
+
+  const bettingRoundSpending = currentBettingRound
+    ? calculateBettingRoundSpendingForPlayer(currentBettingRound, player.id)
+    : 0;
   return (
-    <div className={`player player-${index + 1}`}>
+    <div
+      className={`player player-${
+        index + 1
+      } ${isTurnPlayerClass} ${isAppPlayerClass}`}
+    >
       <span className="status" />
       <div className="info">
         <span className="name">{player.name}</span>
-        <span className="money">{player.money}</span>
+        <div className="d-flex align-items-center">
+          <MonetizationOn className="mr-1" fontSize="small" />
+          <span className="money">{player.money}</span>
+        </div>
+        {!isQuestionRoundOver && !!bettingRoundSpending && (
+          <div className="spending">
+            <MonetizationOn className="mr-1" fontSize="inherit" />
+            <span>{bettingRoundSpending}</span>
+          </div>
+        )}
       </div>
     </div>
   );
--- client/src/components/Game/PokerTable/index.tsx
+++ client/src/components/Game/PokerTable/index.tsx
@@ -1,19 +1,42 @@
 import React from "react";
-import Player from "./Player";
-import { Game } from "../../../interfaces";
+import PlayerComp from "./Player";
+import Question from "../Question";
+import { Game, Player, QuestionRound } from "../../../interfaces";
 
 import "./styles.scss";
+import { getCurrentBettingRound } from "../helpers";
 
 interface Props {
   game: Game;
+  usedQuestionRound?: QuestionRound;
+  playerId?: Player["id"];
 }
 
-const PokerTable = ({ game }: Props) => {
+const PokerTable = ({ game, usedQuestionRound, playerId }: Props) => {
+  const currentBettingRound = getCurrentBettingRound(usedQuestionRound);
   return (
     <div className="poker-table">
       {game.players.map((player, index) => (
-        <Player key={player.id} {...{ player, index }} />
+        <PlayerComp
+          key={player.id}
+          {...{
+            player,
+            index,
+            currentBettingRound,
+            isAppPlayer: player.id === playerId,
+            isTurnPlayer: player.id === currentBettingRound?.currentPlayer.id,
+            isQuestionRoundOver: !!usedQuestionRound?.isOver,
+          }}
+        />
       ))}
+      {usedQuestionRound && (
+        <Question
+          {...{
+            game,
+            usedQuestionRound,
+          }}
+        />
+      )}
     </div>
   );
 };
