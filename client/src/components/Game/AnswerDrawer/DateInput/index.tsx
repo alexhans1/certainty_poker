11698124b9bf96b83008167addff8a339e5e207e
alexhans <mail@alexanderhans.me>
2021-04-01 14:02:45 +0200

    added date picker

--- client/src/components/Game/AnswerDrawer/DateInput/index.tsx
+++ client/src/components/Game/AnswerDrawer/DateInput/index.tsx
@@ -1,11 +1,15 @@
 import React, { useState } from "react";
-
+import DatePicker from "react-date-picker";
 interface Props {
   handleSubmit: (guess: number) => void;
 }
 
-export default ({ handleSubmit }: Props) => {
-  const [guess, setGuess] = useState<string>();
+function formatDateToString(date: Date) {
+  return date.toISOString().slice(0, 10).replaceAll("-", "");
+}
+
+function DateInput({ handleSubmit }: Props) {
+  const [guess, setGuess] = useState<Date>();
 
   const submit = (stringValue?: string) => {
     if (stringValue) {
@@ -19,33 +23,33 @@ export default ({ handleSubmit }: Props) => {
 
   return (
     <div className="grid grid-cols-2 gap-4">
-      <input
+      <DatePicker
+        className="bg-white"
+        dayPlaceholder="DD"
+        monthPlaceholder="MM"
+        yearPlaceholder="YYYY"
+        calendarClassName="hidden"
+        showLeadingZeros={true}
+        calendarIcon={null}
         value={guess}
-        onChange={(e) => {
-          setGuess(e.target.value);
+        onChange={(date: Date | Date[]) => {
+          setGuess(date as Date);
         }}
-        onKeyUp={(e) => {
-          if (e.which === 13) {
-            submit(guess);
-          }
-        }}
-        type="date"
-        className="bg-white border border-gray-400 px-4"
-        placeholder="Your answer"
-        aria-label="Your answer"
-        aria-describedby="basic-addon2"
-        autoFocus
       />
       <button
         type="submit"
         className="bg-blue-500 mr-auto rounded-lg font-bold text-white text-center px-4 py-3 transition duration-300 ease-in-out hover:bg-blue-600"
         disabled={!guess}
         onClick={() => {
-          submit(guess);
+          if (guess) {
+            submit(formatDateToString(guess));
+          }
         }}
       >
         Submit
       </button>
     </div>
   );
-};
+}
+
+export default DateInput;
