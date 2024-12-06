5da6ef7fcd6bafd03429286003701e87ec5aa597
alexhans <mail@alexanderhans.me>
2024-12-06 09:48:52 +0100

    added place bet function

---
+++ functions/placeBet/function.go
@@ -0,0 +1,42 @@
+package placeBet
+
+import (
+	"encoding/json"
+	"net/http"
+
+	"github.com/GoogleCloudPlatform/functions-framework-go/functions"
+	"github.com/alexhans1/certainty_poker/functions/placeBet/handler"
+)
+
+type RequestBody struct {
+	GameID   string `json:"gameId"`
+	PlayerID string `json:"playerId"`
+	Amount   int    `json:"amount"`
+}
+
+func init() {
+	functions.HTTP("placeBet", httpHandler)
+}
+
+// helloWorld writes "Hello, World!" to the HTTP response.
+func httpHandler(w http.ResponseWriter, r *http.Request) {
+	// extract gameId, playerId and amount from request body
+	var body RequestBody
+	if err := json.NewDecoder(r.Body).Decode(&body); err != nil {
+		http.Error(w, "Invalid JSON body", http.StatusBadRequest)
+		return
+	}
+	defer r.Body.Close()
+
+	success, err := handler.PlaceBet(body.GameID, body.PlayerID, body.Amount)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	if success {
+		w.WriteHeader(http.StatusOK)
+	} else {
+		w.WriteHeader(http.StatusInternalServerError)
+	}
+}
---
+++ functions/placeBet/handler/placeBet.go
@@ -0,0 +1,88 @@
+package handler
+
+import (
+	"context"
+	"errors"
+	"log"
+
+	"cloud.google.com/go/firestore"
+	"github.com/alexhans1/certainty_poker/shared/helpers"
+	"github.com/alexhans1/certainty_poker/shared/model"
+	"github.com/sirupsen/logrus"
+)
+
+func PlaceBet(gameID string, playerID string, amount int) (bool, error) {
+	var logger = logrus.New()
+
+	ctx := context.Background()
+
+	// Initialize Firestore client from the Firebase app
+	client, err := firestore.NewClient(ctx, "certainty-poker-ac89b")
+	if err != nil {
+		log.Fatalf("Error initializing Firestore client: %v", err)
+	}
+	defer client.Close()
+
+	game, err := model.FindGame(ctx, client, gameID)
+	if err != nil {
+		return false, err
+	}
+
+	questionRound := game.CurrentQuestionRound()
+	if len(questionRound.Guesses) < len(game.InPlayers()) {
+		return false, errors.New("not all players have submitted their guess yet")
+	}
+
+	if questionRound.CurrentBettingRound().CurrentPlayer.ID != playerID {
+		return false, errors.New("it's not the player's turn")
+	}
+
+	player := game.FindPlayer(playerID)
+
+	if helpers.ContainsString(questionRound.FoldedPlayerIds, player.ID) {
+		return false, errors.New("folded players cannot place another bet in current question round")
+	}
+	if player.ID == playerID && player.Money < amount {
+		return false, errors.New("player does not have enough money to place this bet")
+	}
+
+	bettingRound := questionRound.CurrentBettingRound()
+
+	if amount == -1 {
+		questionRound.Fold(playerID)
+	} else {
+		minimumAmount := bettingRound.AmountToCall() - player.MoneyInQuestionRound()
+		if amount < minimumAmount && player.Money > amount {
+			return false, errors.New("amount is not enough to call and the player is not all in")
+		}
+		newBet := model.Bet{
+			Amount:   amount,
+			PlayerID: playerID,
+		}
+		bettingRound.AddBet(&newBet, false)
+	}
+
+	if bettingRound.IsFinished() {
+		if questionRound.IsFinished() {
+			questionRound.DistributePot()
+			if game.IsFinished() {
+				logger.WithFields(logrus.Fields{
+					"game":               game.ID,
+					"remainingPlayers":   len(game.ActivePlayers()),
+					"currentQuestion":    len(game.QuestionRounds),
+					"remainingQuestions": len(game.Questions),
+				}).Info("game is over")
+			} else {
+				game.AddNewQuestionRound()
+			}
+		} else {
+			questionRound.AddNewBettingRound()
+		}
+	} else {
+		bettingRound.MoveToNextPlayer()
+	}
+
+	game.Save(ctx, client)
+
+	return true, nil
+}
