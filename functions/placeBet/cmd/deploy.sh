548c5f58528d09d058812786e7eb00376382de36
alexhans <mail@alexanderhans.me>
2024-12-07 17:47:26 +0100

    added small blind env variables

--- functions/placeBet/cmd/deploy.sh
+++ functions/placeBet/cmd/deploy.sh
@@ -5,7 +5,7 @@ gcloud functions deploy placeBet \
     --entry-point placeBet \
     --trigger-http \
     --allow-unauthenticated \
-    --set-env-vars GOOGLE_PROJECT_ID="certainty-poker"
+    --set-env-vars GOOGLE_PROJECT_ID="certainty-poker",SMALL_BLIND="5",DOUBLE_EVERY_NTH_ROUND="7"
 
 # Check deployment status
 if [ $? -eq 0 ]; then
