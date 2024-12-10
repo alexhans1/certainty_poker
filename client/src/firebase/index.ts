e13ae8987a162448839e9e2d620d98436c28832f
alexhans <mail@alexanderhans.me>
2024-12-10 20:23:18 +0100

    added app check

--- client/src/firebase/index.ts
+++ client/src/firebase/index.ts
@@ -1,9 +1,13 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
+import {
+  initializeAppCheck,
+  ReCaptchaEnterpriseProvider,
+} from "firebase/app-check";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: import.meta.env.FIREBASE_API_KEY,
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
   authDomain: "certainty-poker.firebaseapp.com",
   projectId: "certainty-poker",
   storageBucket: "certainty-poker.firebasestorage.app",
@@ -12,6 +16,19 @@ const firebaseConfig = {
 };
 
 // Initialize Firebase
-export const app = initializeApp(firebaseConfig);
+const app = initializeApp(firebaseConfig);
+
+// Create a ReCaptchaEnterpriseProvider instance using your reCAPTCHA Enterprise
+// site key and pass it to initializeAppCheck().\
+if (import.meta.env.DEV) {
+  (self as any).VITE_FIREBASE_APPCHECK_DEBUG_TOKEN =
+    import.meta.env.APP_CHECK_DEBUG_TOKEN;
+}
+initializeAppCheck(app, {
+  provider: new ReCaptchaEnterpriseProvider(
+    import.meta.env.VITE_RECAPTCHA_SITE_KEY
+  ),
+  isTokenAutoRefreshEnabled: true, // Set to true to allow auto-refresh.
+});
 
 export default app;
